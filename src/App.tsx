708b06f781e66427613f50e0796d44c9d7430d5d
Daniel Williams <danielfrwilliams@gmail.com>
2024-02-28 00:21:59 -0500

    Add construction literature

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,23 @@ import "./App.css";
 function App() {
   return (
     <>
-      <p>hello world</p>
+      <p>Hi, I'm under construction.</p>
+      <p>
+        View this on{" "}
+        <a href="https://github.com/danielbeans/website" target="_blank">
+          Github
+        </a>
+      </p>
+      <p>
+        View previous versions of this website:{" "}
+        <a href="https://v2.danielfwilliams.com" target="_blank">
+          V2
+        </a>
+        {" - "}
+        <a href="https://v1.danielfwilliams.com" target="_blank">
+          V1
+        </a>
+      </p>
     </>
   );
 }
